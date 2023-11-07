0de5036736efcf3ef017b281284e8706edad1d48
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-06 23:53:57 -0800

    Header component implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+"use client"
 
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="">
       
-      <h1>Lets Build AirBnB!!</h1>
-    </main>
+     <Header/>
+    </div>
   )
 }

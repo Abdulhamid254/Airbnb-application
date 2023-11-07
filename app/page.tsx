c8d967a4fadecca0321dfb745e922416acd81a92
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-07 12:28:47 -0800

    Implemented the Banner & SmallCard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,74 @@
-"use client"
 
 import Banner from "@/components/Banner"
 import Header from "@/components/Header"
+import MediumCard from "@/components/MediumCard";
+import SmallCard from "@/components/SmallCard";
 
-export default function Home() {
+
+export default async function Home() {
+ 
+  const res = await fetch('https://www.jsonkeeper.com/b/4G1G');
+  const data = await res.json();
+  console.log(data);
+
+
+  const cards = await fetch('https://www.jsonkeeper.com/b/VHHT');
+  const cardsData = await cards.json();
+  console.log(cardsData);
+  
+
+
+  
+  
+
   return (
     <div className="">
       
      <Header/>
      <Banner />
+
+
+     <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+       {data.map(({img,distance,location}:any )=> (
+        // eslint-disable-next-line react/jsx-key
+        <SmallCard
+        key={img}
+        img={img}
+        distance={distance}
+        location={location}
+        />
+       ))}
+       </div> 
+      </section>
+
+      <section>
+        <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+        {cardsData?.map(({ img, title}:any) => (
+          <MediumCard key={img} img={img} title={title}/>
+        ))}
+      </section>
+     </main>
     </div>
   )
 }
+
+//static rendering
+
+
+// export async function getStaticProps() {
+//   const res = (await fetch('https://www.jsonkeeper.com/b/4G1G'))
+//   const data = await res.json()
+
+ 
+//   return{
+//     props :{
+//       info : data
+//     }
+//   }
+  
+// }

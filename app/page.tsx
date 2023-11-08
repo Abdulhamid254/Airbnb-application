973ca75ed3f1e3f04bf3b44c8688f961c9c39c69
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-07 22:07:58 -0800

    Footer & medium/small card components implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 import Banner from "@/components/Banner"
+import Footer from "@/components/Footer";
 import Header from "@/components/Header"
+import LargeCard from "@/components/LargeCard";
 import MediumCard from "@/components/MediumCard";
 import SmallCard from "@/components/SmallCard";
 
@@ -47,12 +49,23 @@ export default async function Home() {
 
       <section>
         <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-
+        
+        <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
         {cardsData?.map(({ img, title}:any) => (
           <MediumCard key={img} img={img} title={title}/>
         ))}
+        </div>
       </section>
+
+      <LargeCard 
+      img="https://links.papareact.com/4cj"
+      title="The Greatest Outdoors"
+      description="Whislist Curated by Airbnb."
+      buttonText="Get Inspired"
+      />
      </main>
+
+     <Footer/>
     </div>
   )
 }
